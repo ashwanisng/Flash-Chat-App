6ad7029942d9555129c1ef86ce6554b2316142ad
ashwanisng <ashwanicena5@gmail.com>
2021-02-02 10:30:56 +0530

    flash chat starter app added

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flash_chat/utilities/constants.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flash Chat',
+      theme: ThemeData.dark().copyWith(
+          primaryColor: kBlueColor,
+          textTheme: TextTheme(
+            bodyText1: TextStyle(color: Colors.black),
+          )),
+      home: WelcomeScreen(),
+    );
+  }
+}
