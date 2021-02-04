9652b84a3f609a44e35fbb73557d1d88a8854871
ashwanisng <ashwanicena5@gmail.com>
2021-02-04 20:35:04 +0530

    added all the dependencies of firestore

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -8,6 +8,8 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String email;
+  String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
